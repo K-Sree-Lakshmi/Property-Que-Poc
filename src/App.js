981ef200b8ede8c816b941835818da9aba0bf7d5
SreeLakshmiK23 <sree.lakshmi@nineleaps.com>
2021-03-30 18:43:49 +0530

    [Ckeditor4] feature configuring and using ckeditor4

--- src/App.js
+++ src/App.js
@@ -2,18 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 import QuestionTemplate from './Components/QuestionTemplate';
+import LegalTemplate from './Components/LegalAgreementTemplate/LegalTemplate';
 
 function App() {
   return (
-    <div style={{margin:'25px'}}>
-      {/* <Input placeholder="Question" />
-      <Select style={{ width: 120 }}>
-      <Option value="jack"><PlusOutlined/> Jack</Option>
-      <Option value="lucy"><CloseOutlined/> Lucy</Option>
-      <Option value="Yiminghe"><DeleteOutlined/> yiminghe</Option>
-    </Select>
-    <Checkbox>Checked</Checkbox> */}
-    <QuestionTemplate/>
+    <div style={{ margin: '25px' }}>
+      {/* <QuestionTemplate /> */}
+      <LegalTemplate/>
     </div>
   );
 }
