e3343d1172662432436ce51058d43b3305b33fee
SreeLakshmiK23 <sree.lakshmi@nineleaps.com>
2021-04-01 11:28:29 +0530

    [editor] style design changes

--- src/Components/LegalAgreementTemplate/LegalTemplate.js
+++ src/Components/LegalAgreementTemplate/LegalTemplate.js
@@ -2,14 +2,18 @@ import React, { Component } from 'react'
 import CKEditor from 'ckeditor4-react';
 
 export class LegalTemplate extends Component {
-    editorData(e){
+    editorData(e) {
         console.log(e)
         console.log(e.editor.getData())
     }
     render() {
         return (
             <div>
-                <CKEditor data="<p>This is an example CKEditor 4 WYSIWYG editor instance.</p>"
+                <CKEditor
+                    config={{
+                        height: "500px"
+                    }}
+                    data="<p>This is an example CKEditor 4 WYSIWYG editor instance.</p>"
                     onChange={(e) => this.editorData(e)} />
             </div>
         )
