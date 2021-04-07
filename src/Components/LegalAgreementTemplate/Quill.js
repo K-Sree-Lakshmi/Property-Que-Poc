9f4075245c2a626422bac016a786ad8d2c4fff87
SreeLakshmiK23 <sree.lakshmi@nineleaps.com>
2021-04-07 12:53:33 +0530

    [quill] refactor bubble theme with toolbars

--- src/Components/LegalAgreementTemplate/Quill.js
+++ src/Components/LegalAgreementTemplate/Quill.js
@@ -6,7 +6,7 @@ import './Quill.css';
 export class Quil extends Component {
     constructor(props) {
         super(props)
-        this.state = { text: '', saved: '', visible:false } // You can also pass a Quill Delta here
+        this.state = { text: '', saved: '', visible: false } // You can also pass a Quill Delta here
         this.handleChange = this.handleChange.bind(this)
         this.selectedChange = this.selectedChange.bind(this)
 
@@ -26,13 +26,12 @@ export class Quil extends Component {
         console.log(source, 'source')
         console.log(editor.getContents().ops[0].insert, 'editor')
         this.setState({ saved: editor.getContents().ops[0].insert })
-        if (range.index > 0 && source === 'user') {
+        // if (range.index > 0 && source === 'user') {
+        if (source === 'user') {
+
             console.log('yo')
-                this.setState({visible:!this.state.visible})
+            this.setState({ visible: !this.state.visible })
         }
-        // else if(source==='silent'){
-        //     this.setState({visible:false})
-        // }
 
     }
 
@@ -56,7 +55,7 @@ export class Quil extends Component {
         return (
             <div className='quil__editor-container'>
                 <div>
-                    <ReactQuill
+                    {/* <ReactQuill
                         style={{
                             width: "600px",
                             height: "500px"
@@ -67,16 +66,28 @@ export class Quil extends Component {
                         value={this.state.text}
                         onChange={this.handleChange}
                         onChangeSelection={this.selectedChange}
+                    /> */}
+                    <ReactQuill
+                        style={{
+                            width: "600px",
+                            height: "500px",
+                            border: '1px solid #ccc'
+                        }}
+                        theme="bubble"
+                        modules={modules}
+                        value={this.state.text}
+                        onChange={this.handleChange}
+                    onChangeSelection={this.selectedChange}
                     />
                 </div>
                 <div className="quil-tooltip">
-                    {this.state.visible?
-                    <Tooltip placement="top" title="Add deviation">
-                        <Button type="primary" shape="circle" onClick={()=>alert('hey')}>
-                            A
+                    {this.state.visible ?
+                        <Tooltip placement="top" title="Add deviation">
+                            <Button type="primary" shape="circle" onClick={() => alert('hey')}>
+                                A
                         </Button>
-                    </Tooltip>
-                    : null}
+                        </Tooltip>
+                        : null}
                 </div>
             </div>
         )
